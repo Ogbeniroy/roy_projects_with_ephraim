11e9c3b70ba3bd58ea0a2bab8a159fb690480dbc
ogbeniroy <rajiyusuff25'gmail.com>
2024-04-13 00:05:27 +0100

    moved to main

--- class_1/main.py
+++ class_1/main.py
@@ -1,2 +1,48 @@
-print("hello world")
-print("whats happening")
+def firstbox(height,length:int,steps):
+    end_character = (length + 1)
+    space_character = (length-2)
+    if steps ==1:
+        print("*"* length)
+        for i in range(1,height - 1):
+            print("*"+" "* space_character +"*")
+        print("*"* length)
+        
+    elif steps > 1 and steps <3:
+        print("*"* length)
+        for i in range(1,height - 1):
+            print("*"+" "* space_character +"*")      
+        
+        print("*"+" "* space_character +"*" * end_character)
+        space_character += end_character - 1
+        for i in range(1,height - 1):
+            print("*"+" "* space_character +"*")
+        print("*"* (space_character + 2))
+    
+    elif   steps > 2:
+        print("*"* length)
+        for i in range(1,height - 1):
+            print("*"+" "* space_character +"*")      
+        
+        for stepping in range(7): 
+            print("*"+" "* space_character +"*" * end_character)
+            space_character += end_character - 1
+            for i in range(1,height - 1):
+                print("*"+" "* space_character +"*")
+        
+                
+        print("*"* (space_character + 2))
+            
+            
+            
+            
+            
+       
+
+firstbox(6,7,5)
+firstbox(8,9,6)
+
+
+
+        
+
+
